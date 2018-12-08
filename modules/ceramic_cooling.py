Fix scaling errors causing nan values by using SI unit, add force convection

modules/ceramic_cooling.py
@@ -9,10 +9,10 @@ Created on Tue Dec  4 17:22:14 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-h = 1E-5                    # Step size h (in mm)
-k_m = 0.15                  # Conductivity of silicon Microchip in W/mm K
-k_c = 0.23                  # Conductivity of ceramic block in W/mm K
-del_T = 0.5 / k_m           # Change in temperature of microprocessor every s
+h = 1E-3                    # Step size h (in m)
+k_m = 150                   # Conductivity of silicon Microchip in W/mm K
+k_c = 230                   # Conductivity of ceramic block in W/mm K
+del_T = 0.25 * h ** 2 * 500 * 1E6 / k_m           # Change in temperature of microprocessor every s
 T_a = 20                    # Ambient temperature
 alpha_m = h * 2.62 / k_m    # Constant for natural convection
 alpha_c = h * 2.62 / k_c    # Constant for natural convection
@@ -48,10 +48,8 @@ def update_boundaries(mesh):
 mesh = np.zeros((rows, cols))
 mesh[1, 1:-1] = 20
 mesh[2:-1, 1:-1] = 20      # Initialise the inner mesh with T > T_a
-print(mesh)
 
 mesh = update_boundaries(mesh).copy()
-print(mesh)
 
 all_mesh = []
 
@@ -85,4 +83,5 @@ plt.plot(y, m, "--", c='r')
 plt.figure(2)
 mesh_min = mesh[1:-1, 1:-1].min() - 0.01
 mesh_max = mesh[1:-1, 1:-1].max()
-plt.imshow(mesh, vmin=mesh_min, vmax=mesh_max)
+masked = np.ma.masked_where(mesh < 0.01, mesh)
+plt.imshow(masked, cmap="rainbow")

modules/force_convection.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec  4 22:58:42 2018
+Created on Sat Dec  8 01:53:55 2018
 
 @author: ricktjwong
 """
@@ -9,15 +9,15 @@ Created on Tue Dec  4 22:58:42 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-h = 1E-3                  # Step size h (in m)
-k_m = 150                 # Conductivity of silicon Microchip in W/m K
-k_c = 230                 # Conductivity of ceramic block in W/m K
-k_a = 248                 # Conductivity of aluminium fin in W/m K
-rho = 0.25 * h ** 2 * 500 * 1E6 / k_m  # Change in temperature of microprocessor every s
-T_a = 20                    # Ambient temperature
-alpha_m = h * 2.62 / k_m    # Constant for natural convection for silicon
-alpha_c = h * 2.62 / k_c    # Constant for natural convection for ceramic
-alpha_a = h * 2.62 / k_a    # Constant for natural convection for aluminium
+h = 1E-3                                    # Step size h (in m)
+k_m = 150                                   # Conductivity of silicon Microchip in W/m K
+k_c = 230                                   # Conductivity of ceramic block in W/m K
+k_a = 248                                   # Conductivity of aluminium fin in W/m K
+rho = 0.25 * h ** 2 * 500 * 1E6 / k_m       # Change in temperature of microprocessor every s
+T_a = 20                                    # Ambient temperature
+alpha_m = h * 2 / k_m * (11.54 + 5.7 * 20)  # Constant for force convection for silicon
+alpha_c = h * 2 / k_c * (11.54 + 5.7 * 20)  # Constant for force convection for ceramic
+alpha_a = h * 2 / k_a * (11.54 + 5.7 * 20)  # Constant for force convection for aluminium
 scale = 1
 rows = 35 * scale + 2
 cols = 20 * scale + 2
@@ -42,10 +42,10 @@ def update_boundaries(m, c):
     u_mesh = m[0:3, 1:-1]
     d_mesh = m[rows-3:, 1:-1]
 
-    l_mesh[:,0] = l_mesh[:,-1] - c * (l_mesh[:,1] - T_a) ** (4/3)
-    r_mesh[:,-1] = r_mesh[:,0] - c * (r_mesh[:,1] - T_a) ** (4/3)
-    u_mesh[0] = u_mesh[-1] - c * (u_mesh[1] - T_a) ** (4/3)
-    d_mesh[-1] = d_mesh[0] - c * (d_mesh[1] - T_a) ** (4/3)
+    l_mesh[:,0] = l_mesh[:,-1] - c * (l_mesh[:,1] - T_a)
+    r_mesh[:,-1] = r_mesh[:,0] - c * (r_mesh[:,1] - T_a)
+    u_mesh[0] = u_mesh[-1] - c * (u_mesh[1] - T_a)
+    d_mesh[-1] = d_mesh[0] - c * (d_mesh[1] - T_a)
     
     m[1:-1, 0:3] = l_mesh
     m[1:-1, cols-3:] = r_mesh
@@ -101,7 +101,7 @@ while (True):
                                     + mesh[i][j-1] + mesh[i][j+1])
     update[34*scale+1:35*scale+1, 3*scale+1:17*scale+1] += rho
     if np.mean(update[34*scale+1:35*scale+1, 3*scale+1:17*scale+1]) / \
-       np.mean(mesh[34*scale+1:35*scale+1, 3*scale+1:17*scale+1]) - 1 < 1E-5:
+       np.mean(mesh[34*scale+1:35*scale+1, 3*scale+1:17*scale+1]) - 1 < 1E-6:
         break
     c_mesh, m_mesh, f_mesh = update_all_components(update)
     update = update_mesh(update, c_mesh, m_mesh, f_mesh).copy()
@@ -111,7 +111,6 @@ while (True):
 
 x = []
 for i in all_mesh:
-#    x.append(i[342][34])
     x.append(i[31][1])    
 y = [i for i in range(len(x))]
 

modules/lone_microprocessor.py
@@ -13,11 +13,11 @@ plt.rcParams['lines.linewidth'] = 1.4
 plt.rcParams.update({'figure.autolayout': True})
 plt.rcParams['mathtext.default'] = 'regular'
 
-h = 1E-5                    # Step size h (in mm)
-k = 0.15                    # Conductivity of silicon Microchip in W/mm K
+h = 1E-3                    # Step size h (in mm)
+k = 150                    # Conductivity of silicon Microchip in W/mm K
 T_a = 20                    # Ambient temperature
 alpha = h * 2.62 / k        # Constant for natural convection
-del_T = 0.5 / k             # Change in temperature of microprocessor every s
+del_T = 0.25 * h**2 * 500 * 1E6 / k             # Change in temperature of microprocessor every s
 rows = 4
 cols = 16
 
@@ -42,10 +42,8 @@ def update_boundaries(mesh):
     return mesh
 
 mesh = np.zeros((rows, cols))
-mesh[1:-1, 1:-1] = 20      # Initialise the inner mesh with T > T_a
+mesh[1:-1, 1:-1] = 20      # Initialise the inner mesh with T_a
 mesh = update_boundaries(mesh).copy()
-
-print(mesh)
 
 all_mesh = []
 
@@ -64,8 +62,6 @@ while (n < 20000):
     all_mesh.append(mesh)
     n += 1
 
-print(mesh)
-
 x = []
 m = []
 for i in all_mesh:
@@ -75,7 +71,7 @@ y = [i for i in range(len(m))]
 plt.figure(1)
 #plt.plot(y, x, "--", c='r')
 plt.plot(y, m, "--", c='b')
-plt.savefig('temp_comparison.pdf', format='pdf', dpi=3000)
+#plt.savefig('temp_comparison.pdf', format='pdf', dpi=3000)
 
 plt.figure(2)
 mesh_min = mesh[1:-1, 1:-1].min() - 0.01

modules/square_block.py
@@ -8,13 +8,17 @@ Created on Mon Dec  3 00:47:27 2018
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-h = 1E-5                    # Step size h (in mm)
-k = 0.150                   # Conductivity of silicon Microchip in W/mm K
+h = 1E-3                    # Step size h (in m)
+k = 150                     # Conductivity of silicon Microchip in W/m K
 T_a = 20                    # Ambient temperature
 alpha = h * 2.62 / k        # Constant for natural convection
-rows = 10
-cols = 10
+del_T = 0.25 * h ** 2 * 500 * 1E6 / k
+s = 1                      # Introduce a scaling factor
+rows = 10 * s
+cols = 10 * s
+
 
 def update_boundaries(mesh):
     rows = mesh.shape[0]
@@ -36,28 +40,25 @@ def update_boundaries(mesh):
     return mesh
 
 mesh = np.zeros((rows, cols))
-mesh[1:-1, 1:-1] = 100      # Initialise the inner mesh with T > T_a
+mesh[1:-1, 1:-1] = 20      # Initialise the inner mesh with T > T_a
 mesh = update_boundaries(mesh).copy()
-
-print(mesh)
 
 all_mesh = []
 
 n = 0
-while (True):
+while (n < 100000):
     update = mesh.copy()
     for j in range(1, cols-1):
         for i in range(1, rows-1):
             update[i][j] = 1/4 * (mesh[i-1][j] + mesh[i+1][j] 
                                 + mesh[i][j-1] + mesh[i][j+1])
-    if np.linalg.norm(update[1:-1, 1:-1]/mesh[1:-1, 1:-1] - 1) < 1E-4:
-        break
+    update[4*s:6*s, 4*s:6*s] += del_T
+#    if np.linalg.norm(update[1:-1, 1:-1]/mesh[1:-1, 1:-1] - 1) < 1E-4:
+#        break
     update = update_boundaries(update).copy()
     mesh = update.copy()
     all_mesh.append(mesh)
     n += 1
-
-print(mesh)
 
 x = []
 m = []
@@ -65,10 +66,19 @@ for i in all_mesh:
     x.append(i[1][1])
     m.append(i[1][2])
 y = [i for i in range(len(x))]
+plt.figure(2)
 plt.plot(y, x, "--", c='r')
 plt.plot(y, m, "--", c='b')
 
-mesh_min = mesh[1:-1, 1:-1].min() - 0.01
-mesh_max = mesh[1:-1, 1:-1].max()
+T_min = mesh[1:-1, 1:-1].min() - 0.01
+T_max = mesh[1:-1, 1:-1].max()
 masked = np.ma.masked_where(mesh < 0.01, mesh)
-plt.imshow(masked, cmap='rainbow')
+ax = plt.subplot(111)
+im = ax.imshow(masked, cmap='hot', vmin=T_min, vmax=T_max)
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.05)
+plt.xlabel('$T$ / K', labelpad=20)
+plt.colorbar(im, cax=cax)
+
+#masked = np.ma.masked_where(mesh < 0.01, mesh)
+#plt.imshow(masked, cmap='rainbow')