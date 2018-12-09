c829b5884b1cbd640b9ece4a629c282a864e094e
ricktjwong <rickwtj@gmail.com>
2018-12-09 16:32:40 +0000

    Add comments, generalise methods

--- modules/force_convection.py
+++ modules/force_convection.py
@@ -16,13 +16,17 @@ h = 1E-3 / scale            # Step size h (in m)
 k_m = 150                   # Conductivity of silicon Microchip in W/m K
 k_c = 230                   # Conductivity of ceramic block in W/m K
 k_a = 248                   # Conductivity of aluminium fin in W/m K
-rho = 0.25 * h ** 2 * 500 * 1E6 / k_m  # Change in temperature of microprocessor every s
+# Change in temperature of microprocessor every s
+rho = 0.25 * h ** 2 * 500 * 1E6 / k_m
 T_a = 20                    # Ambient temperature
-alpha_m = h * 2 / k_m * (11.54 + 5.7 * 20)  # Constant for force convection for silicon
-alpha_c = h * 2 / k_c * (11.54 + 5.7 * 20)  # Constant for force convection for ceramic
-alpha_a = h * 2 / k_a * (11.54 + 5.7 * 20)  # Constant for force convection for aluminium
-n_fins = 10
-b = 5
+# Constant for force convection for silicon
+alpha_m = h * 2 / k_m * (11.54 + 5.7 * 20)
+# Constant for force convection for ceramic
+alpha_c = h * 2 / k_c * (11.54 + 5.7 * 20)
+# Constant for force convection for aluminium
+alpha_a = h * 2 / k_a * (11.54 + 5.7 * 20)
+n_fins = 14
+b = 1
 c = 1
 f_h = 30
 T = b+c
@@ -45,28 +49,41 @@ m_idx_y1, m_idx_y2 = c_idx_y2, c_idx_y2 + 1 * scale
 
 
 def setup():
-    mesh = np.zeros((rows, cols))        
+    """
+    Initialise the initial mesh with boundary points
+    """
+    mesh = np.zeros((rows, cols))
+    # Initialise fin mesh points to T_a
     for i in range(n_fins):
-        mesh[f_idx_y1:f_idx_y2, (T*i*scale+1):(c+T*i)*scale+1] = T_a  # Initialise fin 1 to T_a
-    mesh[fb_idx_y1:fb_idx_y2, fb_idx_x1:fb_idx_x2] = T_a              # Initialise the fin block to T_a        
-    mesh[c_idx_y1:c_idx_y2, c_idx_x1:c_idx_x2] = T_a                  # Initialise the ceramic block at T_a
-    mesh[m_idx_y1:m_idx_y2, m_idx_x1:m_idx_x2] = T_a                  # Initialise the microprocessor at T_a
+        mesh[f_idx_y1:f_idx_y2, (T*i*scale+1):(c+T*i)*scale+1] = T_a
+    # Initialise the fin block mesh points to T_a        
+    mesh[fb_idx_y1:fb_idx_y2, fb_idx_x1:fb_idx_x2] = T_a
+    # Initialise the ceramic block mesh points to T_a
+    mesh[c_idx_y1:c_idx_y2, c_idx_x1:c_idx_x2] = T_a
+    # Initialise the microprocessor mesh points to T_a
+    mesh[m_idx_y1:m_idx_y2, m_idx_x1:m_idx_x2] = T_a
     return mesh
 
 
 def update_boundaries(m, c):
+    """
+    Calculate the values for boundaries using the Central Difference Scheme
+    (CDS). Each mesh passed represents a rectangular component of the entire
+    structure
+    """
     rows = m.shape[0]
     cols = m.shape[1]
+    # Extract the non-boundary mesh points for four sides of the block
     l_mesh = m[1:-1, 0:3]
     r_mesh = m[1:-1, cols-3:]
     u_mesh = m[0:3, 1:-1]
     d_mesh = m[rows-3:, 1:-1]
-
+    # Update the boundary points using the CDS for four sides of block
     l_mesh[:,0] = l_mesh[:,-1] - c * (l_mesh[:,1] - T_a)
     r_mesh[:,-1] = r_mesh[:,0] - c * (r_mesh[:,1] - T_a)
     u_mesh[0] = u_mesh[-1] - c * (u_mesh[1] - T_a)
     d_mesh[-1] = d_mesh[0] - c * (d_mesh[1] - T_a)
-    
+    # Update the original mesh with the new boundary values
     m[1:-1, 0:3] = l_mesh
     m[1:-1, cols-3:] = r_mesh
     m[0:3, 1:-1] = u_mesh
@@ -74,6 +91,9 @@ def update_boundaries(m, c):
     
 
 def update_all_components(m):
+    """
+    Update boundaries for each building block of the entire structure
+    """
     c_mesh = m[c_idx_y1-1:c_idx_y2+1, c_idx_x1-1:c_idx_x2+1].copy()
     update_boundaries(c_mesh, alpha_c)
     m_mesh = m[m_idx_y1-2:m_idx_y2+1, m_idx_x1-1:m_idx_x2+1].copy()
@@ -88,13 +108,43 @@ def update_all_components(m):
 
 
 def update_mesh(m, c_mesh, m_mesh, fb_mesh, f_mesh):
+    """
+    Update original mesh with the boundary values of different components. The
+    components are stacked in an order that preserves the correct boundaries.
+    In this case we stack in the order: fin block, fins, ceramic, microchip
+    """
+    # Fin block with all boundaries
     m[fb_idx_y1-1:fb_idx_y2+1, fb_idx_x1-1:fb_idx_x2+1] = fb_mesh.copy()
+    # Fins without the bottom boundary
     for i in range(n_fins):
         m[f_idx_y1-1:f_idx_y2, (T*i*scale):(c+T*i)*scale+2] = \
         f_mesh[i][0:-1].copy()
+    # Ceramic without the top boundary
     m[c_idx_y1:c_idx_y2+1, c_idx_x1-1:c_idx_x2+1] = c_mesh[1:].copy()
+    # Microprocessor without the top boundary
     m[m_idx_y1:m_idx_y2+1, m_idx_x1-1:m_idx_x2+1] = m_mesh[2:].copy()
     return m
+
+
+def update_nonboundaries(m, n):
+    """
+    Update all the mesh points for the structure excluding the boundary values.
+    m is the original mesh, n is the new mesh to be returned
+    """
+    # Update matrix for the ceramic block    
+    n[c_idx_y1:c_idx_y2, c_idx_x1:c_idx_x2] = \
+    m[c_idx_y1:c_idx_y2, c_idx_x1:c_idx_x2]
+    # Update matrix for the microprocessor
+    n[m_idx_y1:m_idx_y2, m_idx_x1:m_idx_x2] = \
+    m[m_idx_y1:m_idx_y2, m_idx_x1:m_idx_x2]
+    # Update matrix for the fin block
+    n[fb_idx_y1:fb_idx_y2, fb_idx_x1:fb_idx_x2] = \
+    m[fb_idx_y1:fb_idx_y2, fb_idx_x1:fb_idx_x2]
+    # Update matrix for the fins
+    for i in range(n_fins):
+        n[f_idx_y1:f_idx_y2, (T*i*scale+1):(c+T*i)*scale+1] = \
+        m[f_idx_y1:f_idx_y2, (T*i*scale+1):(c+T*i)*scale+1]
+
 
 mesh = setup()
 plt.imshow(mesh)
@@ -108,30 +158,18 @@ start = time.time()
 n = 0
 while (True):
     update = mesh.copy()
-    # Define kernel for convolution                                         
+    # Define Jacobi filter kernel for convolution                                         
     kernel = np.array([[0,1,0],
                        [1,0,1],
                        [0,1,0]]) 
-    # Perform 2D convolution with input data and kernel 
+    # Perform 2D convolution with input data and Jacobi filter kernel
     out = signal.convolve2d(mesh, kernel,
                             boundary='wrap', mode='same')/kernel.sum()
-    # Update matrix for the ceramic block    
-    update[c_idx_y1:c_idx_y2, c_idx_x1:c_idx_x2] = \
-    out[c_idx_y1:c_idx_y2, c_idx_x1:c_idx_x2]
-    # Update matrix for the microprocessor
-    update[m_idx_y1:m_idx_y2, m_idx_x1:m_idx_x2] = \
-    out[m_idx_y1:m_idx_y2, m_idx_x1:m_idx_x2]
-    # Update matrix for the fin block
-    update[fb_idx_y1:fb_idx_y2, fb_idx_x1:fb_idx_x2] = \
-    out[fb_idx_y1:fb_idx_y2, fb_idx_x1:fb_idx_x2]
-    # Update matrix for the fins
-    for i in range(n_fins):
-        update[f_idx_y1:f_idx_y2, (T*i*scale+1):(c+T*i)*scale+1] = \
-        out[f_idx_y1:f_idx_y2, (T*i*scale+1):(c+T*i)*scale+1]
+    update_nonboundaries(out, update)
     update[m_idx_y1:m_idx_y2, m_idx_x1:m_idx_x2] += rho
-    if np.mean(update[m_idx_y1:m_idx_y2, m_idx_x1:m_idx_x2]) / \
-       np.mean(mesh[m_idx_y1:m_idx_y2, m_idx_x1:m_idx_x2]) - 1 < 1E-6:
-        break
+    m_mean_temp_1 = np.mean(mesh[m_idx_y1:m_idx_y2, m_idx_x1:m_idx_x2])
+    m_mean_temp_2 = np.mean(update[m_idx_y1:m_idx_y2, m_idx_x1:m_idx_x2])
+    if m_mean_temp_2 / m_mean_temp_1 - 1 < 1E-6: break
     c_mesh, m_mesh, fb_mesh, f_mesh = update_all_components(update)
     update = update_mesh(update, c_mesh, m_mesh, fb_mesh, f_mesh).copy()
     mesh = update.copy()
@@ -149,6 +187,9 @@ plt.figure(2)
 plt.plot(y, x, "--", c='r')
 
 plt.figure(3)
-masked = np.ma.masked_where(mesh < 0.01, mesh)
+new_mesh = np.zeros((rows, cols))
+update_nonboundaries(mesh, new_mesh)
+
+masked = np.ma.masked_where(new_mesh < 0.01, new_mesh)
 plt.imshow(masked, cmap="rainbow")
 plt.show()
