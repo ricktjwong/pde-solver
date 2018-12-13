7092957334408c650e50e2bff5784b2dd0b5cf50
ricktjwong <rickwtj@gmail.com>
2018-12-13 12:56:59 +0000

    Prepare files for VM

--- natural_convection_runs.py
+++ natural_convection_runs.py
@@ -16,26 +16,26 @@ scale, b, c, f_h, n_fins
 """
 
 # Vary n_fins
-final = []
-n_fins = np.arange(5, 65, 5)
-for i in n_fins:
-    hs = hst.HeatStructure(2, 5, 1, 30, i, convection_type="natural")
-    start = time.time()
-    final_temp, n = hs.solve_mesh()
-    end = time.time()
-    final.append([i, final_temp, n, (end - start)])
-np.savetxt("natural_convection_vary_nfins.txt", final)
+#final = []
+#n_fins = np.arange(5, 65, 5)
+#for i in n_fins:
+#    hs = hst.HeatStructure(2, 5, 1, 30, i, convection_type="natural")
+#    start = time.time()
+#    final_temp, n = hs.solve_mesh()
+#    end = time.time()
+#    final.append([i, final_temp, n, (end - start)])
+#np.savetxt("natural_convection_vary_nfins.txt", final)
 
 # Vary fin thickness c
-final = []
-fin_thickness = np.arange(1, 21, 1)
-for i in fin_thickness:
-    hs = hst.HeatStructure(2, 5, i, 30, 5, convection_type="natural")
-    start = time.time()
-    final_temp, n = hs.solve_mesh()
-    end = time.time()
-    final.append([i, final_temp, n, (end - start)])
-np.savetxt("natural_convection_vary_fin_thickness.txt", final)
+#final = []
+#fin_thickness = np.arange(1, 21, 1)
+#for i in fin_thickness:
+#    hs = hst.HeatStructure(2, 5, i, 30, 5, convection_type="natural")
+#    start = time.time()
+#    final_temp, n = hs.solve_mesh()
+#    end = time.time()
+#    final.append([i, final_temp, n, (end - start)])
+#np.savetxt("natural_convection_vary_fin_thickness.txt", final)
 
 # Vary fin height f_h
 final = []
@@ -46,6 +46,7 @@ for i in fin_heights:
     final_temp, n = hs.solve_mesh()
     end = time.time()
     final.append([i, final_temp, n, (end - start)])
+    print(final)
 np.savetxt("natural_convection_vary_fin_height.txt", final)
 
 # Vary fin separation b
@@ -56,5 +57,6 @@ for i in fin_seps:
     start = time.time()
     final_temp, n = hs.solve_mesh()
     end = time.time()
-    final.append([i, final_temp, n, (end - start)])  
+    final.append([i, final_temp, n, (end - start)])
+    print(final)    
 np.savetxt("natural_convection_vary_fin_sep.txt", final)
